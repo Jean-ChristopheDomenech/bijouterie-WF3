70e576fd3593c72ff028cc55ba0c5fbdd52a5f26
jean-christophedomenech <jeandomenech@gmail.com>
2021-07-15 16:23:25 +0200

    15/07

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+
+    }
+
+
+
+
+
+
+}
